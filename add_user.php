aaeb7ed2c27e30fcfab14ab82aea78cc28670c07
Dear <deaapiwich@gmail.com>
2026-02-05 14:07:43 +0700

    เว้นช่องว่างระว่างปุ่มบันทึกข้อมูลและปุ่มยกเลิก

--- add_user.php
+++ add_user.php
@@ -48,8 +48,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <option value="admin">Admin</option>
             </select>
         </div>
-        <button type="submit" class="btn btn-success">บันทึกข้อมูล</button>
-        <a href="account.php" class="btn btn-secondary">ยกเลิก</a>
+        <button type="submit" class="btn btn-success">บันทึกข้อมูล</button> <br>
+        <a href="account.php" class="btn btn-secondary">ยกเลิก</a> <br>
     </form>
 </body>
 </html>
